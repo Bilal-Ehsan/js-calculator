a1b051bae3997c60056065154dd629b99db50939
Bilal Ehsan <bilalehsan933@gmail.com>
2021-03-10 22:11:04 +0000

    Added functionality

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 'using strict';
 
+const screen = document.getElementById('screen');
 
+document.querySelectorAll('.grid-item').forEach(item => {
+    item.addEventListener('click', function() {
+        if(item.innerText !== '=') screen.innerText += item.innerText;
+        if(item.innerText === '=') screen.innerText = eval(screen.innerText); // Evaluates string expression
+        if(item.innerText === 'AC') screen.innerText = '';
+        if(item.innerText === 'del') screen.innerText = screen.innerText.slice(0, -4); // Removes 'del' as well as last char
+    });
+});
